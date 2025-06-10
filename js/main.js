591f9e0b301cfce871a6f6a8f933543e3332202e
Dazaa1 <marouanelankaoui066@gmail.com>
2025-06-10 12:56:09 +0100

    Finish Add new todo feature.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 const userInput = document.querySelector("#user-input");
 const addButton = document.querySelector("#add-button");
+const ul = document.createElement('ul');
+const body = document.querySelector('body');
+
+
+// add a click event to the button
+addButton.addEventListener("click", () => {
+    const li = document.createElement('li');
+    li.textContent = userInput.value;
+    userInput.value = "";
+    ul.appendChild(li);
+    body.appendChild(ul);
+})
+
