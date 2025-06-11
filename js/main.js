a191988206f038bccb841397eca0e98f5bafbe50
Dazaa1 <211529624+Dazaa1@users.noreply.github.com>
2025-06-11 17:10:26 +0100

    Add change to add task feature Check if the input is empty and provide a console.log message if it is and don't write anything to the dom if it is.

--- js/main.js
+++ js/main.js
@@ -3,13 +3,29 @@ const addButton = document.querySelector("#add-button");
 const ul = document.createElement('ul');
 const body = document.querySelector('body');
 
+const editButton = document.createElement('button');
+const removeButton = document.createElement('button');
+
+editButton.textContent = ""
+
+
 
 // add a click event to the button
 addButton.addEventListener("click", () => {
     const li = document.createElement('li');
+
     li.textContent = userInput.value;
-    userInput.value = "";
-    ul.appendChild(li);
-    body.appendChild(ul);
+    if (!li.textContent) {
+        console.log("empty string");
+    }
+    
+    else {
+        li.appendChild(editButton);
+        li.appendChild(removeButton);
+        userInput.value = "";
+        ul.appendChild(li);
+        body.appendChild(ul);
+    }
+
 })
 
