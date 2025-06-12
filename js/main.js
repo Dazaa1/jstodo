981bf5caf166ef3cbd264ace649e404ff381adc6
Dazaa1 <211529624+Dazaa1@users.noreply.github.com>
2025-06-12 18:31:34 +0100

    Add little change

--- js/main.js
+++ js/main.js
@@ -1,34 +1,26 @@
 const userInput = document.querySelector("#user-input");
 const addButton = document.querySelector("#add-button");
-const ul = document.createElement('ul');
+const ul = document.querySelector('ul');
 const body = document.querySelector('body');
 
-const editButton = document.createElement('button');
-const removeButton = document.createElement('button');
-
-editButton.textContent = "Edit";
-removeButton.textContent = "Remove";
-
-const createListItems = () => {
-    const li = document.createElement('li');
-    li.textContent = userInput.value;
-    li.appendChild(editButton);
-    li.appendChild(removeButton);
-    return li;
-}
+const li = document.querySelector('li');
+const editButton = document.querySelector('button');
+const removeButton = document.querySelector('button');
 
 const checkEmptyString = (li) => {
-    if (!li.textContent) {
+    if (!li) {
         console.log("empty string");
     }
 }
+
 // add a click event to the button
 addButton.addEventListener("click", () => {
-    li = createListItems();
+    li.textContent = userInput.value;
+    checkEmptyString(li.textContent)
     checkEmptyString(li);    
-    
     userInput.value = "";
     ul.appendChild(li);
+
     body.appendChild(ul);
     
 })
